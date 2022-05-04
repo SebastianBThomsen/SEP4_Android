5d435b1b491788a76e23f188e02d25aff0a81649
Sebastian <280301@via.dk>
2022-05-04 21:56:44 +0200

    API_ALMOST_WORKING (

--- app/src/main/java/com/example/sep4_android/webService/HealthAPI.java
+++ app/src/main/java/com/example/sep4_android/webService/HealthAPI.java
@@ -7,6 +7,8 @@ import retrofit2.http.GET;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
+//https://run.mocky.io/                 v3/6f64b188-00ea-44b3-abaa-387588645afa
+
 public interface HealthAPI {
     @GET("v3/{search}")
     Call<HealthData> getRandomTemperature(@Path("search") String search);
