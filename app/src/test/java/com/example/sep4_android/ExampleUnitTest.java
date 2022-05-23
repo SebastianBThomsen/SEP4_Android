b3011b74ebf5f008df82dc8392a3a3ced0bbeee8
falken123321 <kasper.falk@live.dk>
2022-05-23 14:09:54 +0200

    Unit test start

--- app/src/test/java/com/example/sep4_android/ExampleUnitTest.java
+++ app/src/test/java/com/example/sep4_android/ExampleUnitTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import com.example.sep4_android.model.persistence.entities.Device;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -14,5 +16,20 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+
+
+
+
+
+
+    //DEVICE Test
+    public void deviceTest() {
+        Device testDevice = new Device("Device101", "CO.2");
+        assertTrue(testDevice.getDeviceId() == "Device101");
+
+
+    }
+
 }
 
