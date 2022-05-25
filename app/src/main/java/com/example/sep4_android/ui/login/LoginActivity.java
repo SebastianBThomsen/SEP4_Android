d409d6da73c5382898b2f89bdb8621105d3e5f1e
Andre <36155981+Neaxic@users.noreply.github.com>
2022-05-25 13:28:19 +0200

    login attempt - andreas

---
+++ app/src/main/java/com/example/sep4_android/ui/login/LoginActivity.java
@@ -0,0 +1,75 @@
+package com.example.sep4_android.ui.login;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.sep4_android.R;
+import com.example.sep4_android.databinding.ActivityLoginBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener  {
+    private FirebaseAuth mAuth;
+
+    LoginViewModel viewModel;
+
+    private EditText username;
+    private EditText password;
+
+    private ActivityLoginBinding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.usernameField);
+        password = findViewById(R.id.passwordField);
+        Button b = findViewById(R.id.loginBtn);
+        Button r = findViewById(R.id.signupBtn);
+        r.setOnClickListener(this);
+        b.setOnClickListener(this);
+
+        viewModel = new ViewModelProvider(this).get(LoginViewModelImpl.class);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void onStart() {
+        super.onStart();
+
+        FirebaseUser u = mAuth.getCurrentUser();
+        if(u != null)
+            Toast.makeText(getApplicationContext(), "User already logged in", Toast.LENGTH_LONG).show();
+        else
+            Toast.makeText(getApplicationContext(), "User not logged in", Toast.LENGTH_LONG).show();
+    }
+
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.loginBtn:
+                viewModel.login(LoginActivity.this, username.getText().toString(), password.getText().toString());
+
+                //Explicit intent
+                //Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+
+
+                //startActivity(intent);
+
+                break;
+            case R.id.signupBtn:
+                viewModel.signUp(LoginActivity.this, username.getText().toString(), password.getText().toString());
+                break;
+        }
+    }
+}
