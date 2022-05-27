5e771eda675dd9bae035c4475125a0e084967c31
Andre <36155981+Neaxic@users.noreply.github.com>
2022-05-27 16:34:20 +0200

    icons names & andet lort jeg sikkert har glemt... + dyn nav header

--- app/src/main/java/com/example/sep4_android/ui/mainActivity/MainActivity.java
+++ app/src/main/java/com/example/sep4_android/ui/mainActivity/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -42,7 +43,6 @@ public class MainActivity extends AppCompatActivity {
 
         //(Vi skal åbenbart have url med i getInstance over en en firebase bug (Noget med region lock))
         database = FirebaseDatabase.getInstance("https://sep4-26d6b-default-rtdb.europe-west1.firebasedatabase.app/");
-
         viewModel = new ViewModelProvider(this).get(MainActivityViewModelImpl.class);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
@@ -68,9 +68,8 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-//        hideNavItems();
 
-        //Signout logik (TODO:: Viewmodel maybe)
+        //Signout logik
         FirebaseAuth.AuthStateListener authStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth mAuth) {
@@ -83,9 +82,19 @@ public class MainActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         mAuth.addAuthStateListener(authStateListener);
 
-        hideNavItems();
+        CustomizeNavigation();
     }
 
+    private void setNavHeader(NavigationView navigationView){
+        //Set navbar email
+        View headerView = navigationView.getHeaderView(0);
+        TextView navHeaderMail = headerView.findViewById(R.id.NavHeaderMail);
+        TextView navHeaderDevice = headerView.findViewById(R.id.NavHeaderDevice);
+
+        if(mAuth.getCurrentUser().getEmail() != null){
+            navHeaderMail.setText(mAuth.getCurrentUser().getEmail());
+        }
+    }
 
 
 
@@ -111,14 +120,26 @@ public class MainActivity extends AppCompatActivity {
                 || super.onSupportNavigateUp();
     }
 
-    private void hideNavItems(){
+    private void CustomizeNavigation(){
         //Depending on your role some items will be hidden from the navbar
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         Menu navMenu = navigationView.getMenu();
+
+        //Hiding user role related items
         viewModel.DynamicNavigation(navMenu);
+
+        //Set navbar email
+        View headerView = navigationView.getHeaderView(0);
+        TextView navHeaderMail = headerView.findViewById(R.id.NavHeaderMail);
+        TextView navHeaderDevice = headerView.findViewById(R.id.NavHeaderDevice);
+
+        if(mAuth.getCurrentUser().getEmail() != null){
+            navHeaderMail.setText(mAuth.getCurrentUser().getEmail());
+        }
 
         viewModel.getSelectedDeviceLive().observe(this, device -> {
             viewModel.updateGraphNav(device, navMenu);
+            navHeaderDevice.setText("Selected: "+device.getRoomName());
         });
     }
 
--- app/src/main/java/com/example/sep4_android/ui/mainActivity/MainActivityViewModelImpl.java
+++ app/src/main/java/com/example/sep4_android/ui/mainActivity/MainActivityViewModelImpl.java
@@ -1,19 +1,32 @@
 package com.example.sep4_android.ui.mainActivity;
 
 
+import android.app.Activity;
 import android.app.Application;
+import android.content.Intent;
 import android.view.Menu;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.DefaultLifecycleObserver;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleObserver;
+import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.OnLifecycleEvent;
+import androidx.lifecycle.ViewModel;
 
 import com.example.sep4_android.R;
 import com.example.sep4_android.model.persistence.entities.Device;
 import com.example.sep4_android.repositories.RouteRepository;
 import com.example.sep4_android.repositories.RouteRepositoryImpl;
+import com.example.sep4_android.ui.login.LoginViewModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
