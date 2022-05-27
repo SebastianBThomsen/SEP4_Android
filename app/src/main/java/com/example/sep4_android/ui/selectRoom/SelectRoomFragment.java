2e430d886e87e6828329d03935adb5f21d99a743
Andre <36155981+Neaxic@users.noreply.github.com>
2022-05-27 15:36:29 +0200

    toast add lol

--- app/src/main/java/com/example/sep4_android/ui/selectRoom/SelectRoomFragment.java
+++ app/src/main/java/com/example/sep4_android/ui/selectRoom/SelectRoomFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -67,7 +68,8 @@ public class SelectRoomFragment extends Fragment {
 
             adapter.setOnClickListener(device -> {
                 viewModel.setSelectedDevice(device);
-                Navigation.findNavController(root).navigate(R.id.nav_healthInspection);
+                Toast.makeText(getContext(), "Room slected!", Toast.LENGTH_SHORT).show();
+//                Navigation.findNavController(root).navigate(R.id.nav_healthInspection);
             });
         });
     }
